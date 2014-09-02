672799f7bbe39152369dd1c185388b0e0d59bb6d
Baohua Yang <yangbaohua@gmail.com>
2014-09-02 10:27:03 +0800

    Remove unused options, and set necessary options as cli options

--- heatgen/util/config.py
+++ heatgen/util/config.py
@@ -3,6 +3,7 @@ __author__ = 'baohua'
 from gettext import gettext as _
 
 from oslo.config import cfg
+from heatgen import VERSION
 
 
 default_opts = [
@@ -31,6 +32,7 @@ default_opts = [
                default='127.0.0.1',
                help='IP address of the compute node'),
 ]
+cfg.CONF.register_cli_opts(default_opts)
 cfg.CONF.register_opts(default_opts)
 
 auth_opts = [
@@ -47,10 +49,12 @@ auth_opts = [
                default='admin',
                help='the tenant name to check'),
 ]
+cfg.CONF.register_cli_opts(auth_opts, "AUTH")
+cfg.CONF.register_cli_opts(auth_opts, "PROJECT")
 cfg.CONF.register_opts(auth_opts, "AUTH")
 cfg.CONF.register_opts(auth_opts, "PROJECT")
 
-middlebox_opts = [
+trans_mb_opts = [
     cfg.StrOpt('id',
                 default='',
                 help=_("id of the middlebox instance")),
@@ -62,6 +66,18 @@ middlebox_opts = [
                default='transparent',
                choices=['transparent','routed'],
                help=_("mode of the middlebox instance")),
+    cfg.StrOpt('ha_mode',
+               default='NONE',
+               help=_("the mode for HA")),
+    cfg.StrOpt('interface_type',
+               choices=['one_arm', 'two_arm'],
+               help=_("the type of interface")),
+    cfg.StrOpt('required',
+               default='no',
+               help=_("whether the middlebox instance is required")),
+    cfg.BoolOpt('health_check',
+                default=False,
+                help=_("whether do the health check")),
     cfg.StrOpt('ingress_node',
                default='',
                help=_("ingress switch dpid for the middlebox instance")),
@@ -72,15 +88,6 @@ middlebox_opts = [
     cfg.StrOpt('ingress_port',
                default='',
                help=_("ingress switch port for the middlebox instance")),
-    cfg.StrOpt('ingress_gw_addr',
-               default='',
-               help=_("ingress gateway ip for the middlebox instance")),
-    cfg.StrOpt('ingress_mac_addr',
-               default='',
-               help=_("ingress mac addr for the middlebox instance")),
-    cfg.StrOpt('ingress_cidr',
-               default='',
-               help=_("ingress ip cidr for the middlebox instance")),
     cfg.StrOpt('egress_ip',
                default='',
                help=_("fake field to get port of the transparent middlebox "
@@ -91,6 +98,41 @@ middlebox_opts = [
     cfg.StrOpt('egress_port',
                default='',
                help=_("egress switch port for the middlebox instance")),
+]
+
+routed_mb_opts = [
+    cfg.StrOpt('id',
+                default='',
+                help=_("id of the middlebox instance")),
+    cfg.StrOpt('service_type',
+               default='firewall',
+               choices=['firewall','ips'],
+               help=_("type of the middlebox instance")),
+    cfg.StrOpt('mode',
+               default='transparent',
+               choices=['transparent','routed'],
+               help=_("mode of the middlebox instance")),
+    cfg.StrOpt('ha_mode',
+               default='NONE',
+               help=_("the mode for HA")),
+    cfg.StrOpt('interface_type',
+               choices=['one_arm', 'two_arm'],
+               help=_("the type of interface")),
+    cfg.StrOpt('required',
+               default='no',
+               help=_("whether the middlebox instance is required")),
+    cfg.BoolOpt('health_check',
+                default=False,
+                help=_("whether do the health check")),
+    cfg.StrOpt('ingress_gw_addr',
+               default='',
+               help=_("ingress gateway ip for the middlebox instance")),
+    cfg.StrOpt('ingress_mac_addr',
+               default='',
+               help=_("ingress mac addr for the middlebox instance")),
+    cfg.StrOpt('ingress_cidr',
+               default='',
+               help=_("ingress ip cidr for the middlebox instance")),
     cfg.StrOpt('egress_gw_addr',
                default='',
                help=_("egress gateway ip for the middlebox instance")),
@@ -100,49 +142,14 @@ middlebox_opts = [
     cfg.StrOpt('egress_cidr',
                default='',
                help=_("egress ip cidr for the middlebox instance")),
-    cfg.StrOpt('ha_mode',
-               default='NONE',
-               help=_("the mode for HA")),
-    cfg.StrOpt('interface_type',
-                choices=['one_arm', 'two_arm'],
-                help=_("the type of interface")),
-    cfg.StrOpt('required',
-               default='no',
-               help=_("whether the middlebox instance is required")),
-    cfg.BoolOpt('health_check',
-               default=False,
-               help=_("whether do the health check")),
 ]
 
-MIDDLEBOXES = ['trans_mb', 'routed_mb']
+cfg.CONF.register_cli_opts(trans_mb_opts, 'trans_mb')
+cfg.CONF.register_opts(trans_mb_opts, 'trans_mb')
+cfg.CONF.register_cli_opts(routed_mb_opts, 'routed_mb')
+cfg.CONF.register_opts(routed_mb_opts, 'routed_mb')
 
-for e in MIDDLEBOXES:
-    cfg.CONF.register_opts(middlebox_opts, e)
-
-network_opts = [
-    cfg.StrOpt('id',
-               default='',
-               help=_("id of the network instance")),
-]
-
-NETWORKS = ['net_int1', 'net_int2']
-for e in NETWORKS:
-    cfg.CONF.register_opts(network_opts, e)
-
-
-cli_opts = [
-    cfg.StrOpt('src',
-                default='',
-                help='name of the src endpoint for the policy'),
-    cfg.StrOpt('dst',
-                default='',
-                help='name of the dst endpoint for the policy'),
-    cfg.ListOpt('services',
-               default=[],
-               help='names of the service middleboxes for the policy'),
-    cfg.BoolOpt('deploy',
-                default=False,
-                help='Whether to deploy the rules after generation'),
-]
-
-cfg.CONF.register_cli_opts(cli_opts)
+def init(args, **kwargs):
+    cfg.CONF(args=args, project='heatgen',
+             version='%%prog %s' % VERSION,
+             **kwargs)
